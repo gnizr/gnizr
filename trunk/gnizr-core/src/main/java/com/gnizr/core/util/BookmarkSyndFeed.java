41969b75960977f97f6f23c3b9bf5ebb81c5d9d0
harry.chen <harry.chen@df594a80-b839-0410-a48c-17488be76373>
2007-11-01 15:39:38 +0000

    Added OpenSearch ROME API. Init impl of BookmarkSyndFeed.java for generating a SyndFeed object from a list of Bookmark. BookmarkSyndFeed.java provides a static method for adding OpenSearchModule to an existing SyndFeed object.

---
+++ trunk/gnizr-core/src/main/java/com/gnizr/core/util/BookmarkSyndFeed.java
@@ -0,0 +1,145 @@
+/*
+ * gnizr is a trademark of Image Matters LLC in the United States.
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License
+ * for the specific language governing rights and limitations under the License.
+ * 
+ * The Initial Contributor of the Original Code is Image Matters LLC.
+ * Portions created by the Initial Contributor are Copyright (C) 2007
+ * Image Matters LLC. All Rights Reserved.
+ */
+package com.gnizr.core.util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.gnizr.db.dao.Bookmark;
+import com.sun.syndication.feed.atom.Link;
+import com.sun.syndication.feed.module.Module;
+import com.sun.syndication.feed.module.opensearch.OpenSearchModule;
+import com.sun.syndication.feed.module.opensearch.impl.OpenSearchModuleImpl;
+import com.sun.syndication.feed.synd.SyndCategory;
+import com.sun.syndication.feed.synd.SyndCategoryImpl;
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndFeedImpl;
+
+/**
+ * An utility class for creating ROME {@link SyndFeed}} object from a list of {@link Bookmark}. 
+ * 
+ * @author Harry Chen
+ *
+ */
+public class BookmarkSyndFeed {
+
+	private static final Logger logger = Logger.getLogger(BookmarkSyndFeed.class);
+	private List<Bookmark> bookmarks;
+	private String author;
+	private String title;
+	private String feedUri;
+	private Date pubDate;
+	
+	private SyndFeed syndFeed;
+	
+	public BookmarkSyndFeed(List<Bookmark> bookmarks, String author, String title, String link, Date pubDate, String feedUri){
+		logger.debug("BookmarkSyndFeed constructor called.");
+		this.author = author;
+		logger.debug("author="+this.author);
+		this.title = title;
+		logger.debug("title="+this.title);
+		this.feedUri = feedUri;
+		logger.debug("feedUri="+this.feedUri);
+		if(pubDate == null){
+			this.pubDate = GnizrDaoUtil.getNow();
+		}else{
+			this.pubDate = pubDate;
+		}
+		logger.debug("pubDate="+this.pubDate);
+		if(bookmarks == null){
+			this.bookmarks = new ArrayList<Bookmark>();
+		}else{
+			this.bookmarks = bookmarks;
+		}
+		logger.debug("bookmarks="+this.bookmarks);
+	}
+	
+	public SyndFeed getFeed(){
+		if(syndFeed == null){			
+			initSyndFeed();
+		}
+		return syndFeed;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static SyndFeed addOpenSearchModule(SyndFeed feed, int itemsPerPage, int startIdx, int totalResult, String searchDescriptionUrl){
+		if(feed == null){
+			throw new NullPointerException("feed is NULL");
+		}
+		List<Module> mods = null;
+		mods = feed.getModules();
+		if(mods == null){
+			mods = new ArrayList<Module>();
+		}
+		OpenSearchModule osm = new OpenSearchModuleImpl();
+		osm.setItemsPerPage(itemsPerPage);
+		osm.setStartIndex(startIdx);
+		osm.setTotalResults(totalResult);
+		if(searchDescriptionUrl != null){
+			Link link = new Link();
+			link.setHref(searchDescriptionUrl);
+			link.setType("application/opensearchdescription+xml");
+			osm.setLink(link);
+		}
+		mods.add(osm);
+		feed.setModules(mods);
+		return feed;
+	}
+	
+	private void initSyndFeed(){
+		logger.debug("initialize syndFeed object for the first time");
+		syndFeed = new SyndFeedImpl();
+		syndFeed.setAuthor(author);
+		syndFeed.setTitle(title);
+		syndFeed.setUri(feedUri);
+		syndFeed.setPublishedDate(pubDate);
+		List<SyndEntry> entries = new ArrayList<SyndEntry>();
+		for(Bookmark bmark : bookmarks){
+			SyndEntry entry = new SyndEntryImpl();
+			entry.setTitle(bmark.getTitle());
+			entry.setAuthor(bmark.getUser().getUsername());
+			entry.setLink(bmark.getLink().getUrl());
+			entry.setUri("urn:bookmark:"+bmark.getId());
+			entry.setUpdatedDate(bmark.getLastUpdated());
+			List<String> tags = bmark.getTagList();
+			List<SyndCategory> cats = new ArrayList<SyndCategory>();
+			for(String aTag : tags){
+				SyndCategory aCat = new SyndCategoryImpl();
+				aCat.setName(aTag);
+				cats.add(aCat);
+			}
+			if(cats.isEmpty() == false){
+				entry.setCategories(cats);
+			}
+			SyndContent cnt = new SyndContentImpl();
+			cnt.setType("text/html");
+			cnt.setValue(bmark.getNotes());
+			entry.setDescription(cnt);
+			entries.add(entry);			
+		}
+		syndFeed.setEntries(entries);
+		syndFeed.setEncoding("UTF-8");
+		logger.debug("done initializing syndFeed object");
+	}
+}
---
+++ trunk/gnizr-core/src/test/java/com/gnizr/core/util/TestBookmarkSyndFeed.java
@@ -0,0 +1,97 @@
+package com.gnizr.core.util;
+
+import java.util.Date;
+import java.util.List;
+
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+
+import com.gnizr.core.GnizrCoreTestBase;
+import com.gnizr.db.dao.Bookmark;
+import com.gnizr.db.dao.DaoResult;
+import com.gnizr.db.dao.User;
+import com.gnizr.db.dao.bookmark.BookmarkDao;
+import com.sun.syndication.feed.module.opensearch.OpenSearchModule;
+import com.sun.syndication.feed.synd.SyndCategory;
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+
+public class TestBookmarkSyndFeed extends GnizrCoreTestBase {
+	
+	private List<Bookmark> bmarks = null;
+	private String title = "TITLE STRING FOOBAR";
+	private String link = "http://foo.com/gnizr";
+	private Date pubDate = GnizrDaoUtil.getNow();
+	private String feedUri = "urn:x-id-101";
+	private String author = "gnizr";
+	private int itemsPerPage = 10;
+	private int startIndex = 0;
+	private int totalResult = -1;
+	private String searchDescriptionUrl = "http://foo.com/gnizr/search-description.xml";
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		BookmarkDao bmarkDao = getGnizrDao().getBookmarkDao();
+		DaoResult<Bookmark> result = bmarkDao.pageBookmarks(new User(1),0,10);		
+		bmarks = result.getResult();
+		assertFalse(bmarks.isEmpty());
+		totalResult = result.getSize();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSet(TestBookmarkSyndFeed.class.getResourceAsStream("/TestBookmarkSyndFeed-input.xml"));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void testGetFeed() throws Exception{				
+		BookmarkSyndFeed bsf = new BookmarkSyndFeed(bmarks,author,title,link,pubDate,feedUri);
+		SyndFeed syndFeed = bsf.getFeed();
+		
+		assertEquals(author,syndFeed.getAuthor());
+		assertEquals(title,syndFeed.getTitle());
+		assertEquals(pubDate,syndFeed.getPublishedDate());
+		assertEquals(feedUri,syndFeed.getUri());
+		
+		List<SyndEntry> entries = syndFeed.getEntries();
+		assertEquals(1,entries.size());
+		
+		SyndEntry entry = entries.get(0);
+		assertEquals("http://zirr.us/",entry.getLink());
+		assertEquals("zirrus",entry.getTitle());
+		assertEquals("gnizr",entry.getAuthor());
+		SyndContent cont = entry.getDescription();
+		assertNotNull(cont);
+		assertEquals("website",cont.getValue());
+		assertEquals("text/html",cont.getType());
+		
+		List<SyndCategory> cats = entry.getCategories();
+		assertFalse(cats.isEmpty());
+		SyndCategory cat = cats.get(0);
+		assertEquals("cnn",cat.getName());
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testAddOpenSearchModule() throws Exception{
+		BookmarkSyndFeed bsf = new BookmarkSyndFeed(bmarks,author,title,link,pubDate,feedUri);
+		SyndFeed syndFeed = bsf.getFeed();
+		assertNotNull(syndFeed);
+		
+		syndFeed = BookmarkSyndFeed.addOpenSearchModule(syndFeed, itemsPerPage, 
+				startIndex, totalResult, searchDescriptionUrl);
+	
+		OpenSearchModule osm = (OpenSearchModule) syndFeed.getModule(OpenSearchModule.URI);
+		assertEquals(itemsPerPage,osm.getItemsPerPage());
+		assertEquals(totalResult,osm.getTotalResults());
+		assertEquals(startIndex,osm.getStartIndex());
+		assertEquals(searchDescriptionUrl,osm.getLink().getHref());
+	}
+	
+	
+	
+}
