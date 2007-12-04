ee26aeafb9bfdf9e3a6bd03ddc8be7ba06275c46
harry.chen <harry.chen@df594a80-b839-0410-a48c-17488be76373>
2007-12-04 19:55:34 +0000

    renamed gnizr.ext.delicious to gnizr.core.delicious

--- trunk/gnizr-delicious/src/main/java/com/gnizr/core/delicious/DeliciousImport.java
+++ trunk/gnizr-delicious/src/main/java/com/gnizr/core/delicious/DeliciousImport.java
@@ -14,7 +14,7 @@
  * Portions created by the Initial Contributor are Copyright (C) 2007
  * Image Matters LLC. All Rights Reserved.
  */
-package com.gnizr.ext.delicious;
+package com.gnizr.core.delicious;
 
 import java.util.ArrayList;
 import java.util.Date;
