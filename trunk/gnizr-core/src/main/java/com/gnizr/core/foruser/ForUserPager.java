96dec92f9b295e6781fbb34576ad2d1945773d6f
harry.chen <harry.chen@df594a80-b839-0410-a48c-17488be76373>
2007-12-04 19:19:39 +0000

    moved ForUserPager into core.foruser

--- trunk/gnizr-core/src/main/java/com/gnizr/core/foruser/ForUserPager.java
+++ trunk/gnizr-core/src/main/java/com/gnizr/core/foruser/ForUserPager.java
@@ -14,7 +14,7 @@
  * Portions created by the Initial Contributor are Copyright (C) 2007
  * Image Matters LLC. All Rights Reserved.
  */
-package com.gnizr.core.pagers;
+package com.gnizr.core.foruser;
 
 import java.io.Serializable;
 import java.util.Date;
