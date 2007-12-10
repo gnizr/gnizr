391d3ab994d588ee3568529050df74241e900e08
harry.chen <harry.chen@df594a80-b839-0410-a48c-17488be76373>
2007-12-10 21:24:47 +0000

    added javadoc for class methods.

--- gnizr-db/src/main/java/com/gnizr/db/dao/GnizrDao.java
+++ gnizr-db/src/main/java/com/gnizr/db/dao/GnizrDao.java
@@ -42,7 +42,11 @@ import com.gnizr.db.dao.user.UserDBDao;
 import com.gnizr.db.dao.user.UserDao;
 
 /**
- * A convenient class that wraps DAO objects for access gnizr persistent store. 
+ * A convenient class that wraps DAO objects for access gnizr persistent store. There are two ways to create
+ * an instance of this class: (1) via the static class method <code>GnizrDao.getInstance</code> and (2) 
+ * use the default constructor and explicitly set individual DAO objects to be wrapped. Objects created via 
+ * (1) will be configured to wrap a default of DAO objects. 
+ *  
  * <p>To create an instance of this class:</p>
  * <p>
  * <code>
@@ -74,7 +78,10 @@ public class GnizrDao implements Serializable{
 	private static GnizrDao gnizrDao;
 	
 	/**
-	 * Creates a singlton of this class using the defined connection object <code>datasource</code>  
+	 * Creates a singlton of this class using the defined connection object <code>datasource</code>. This singlton 
+	 * class will wrap a default set of DAO objects. Clients can expect all <code>get</code> methods of this class
+	 * will return instantiated DAO objects. 
+	 *  
 	 * @param datasource an instantiated connection object for accessing persistent store. 
 	 * @return an object this class
 	 */
@@ -95,18 +102,38 @@ public class GnizrDao implements Serializable{
 		return gnizrDao;
 	}
 	
+	/**
+	 * Returns a DAO object for tag assertion operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public TagAssertionDao getTagAssertionDao() {
 		return tagAssertionDao;
 	}
 
+	/**
+	 * Sets the DAO object for tag assertion operations.
+	 * 
+	 * @param tagAssertionDao an instantiated DAO object
+	 */
 	public void setTagAssertionDao(TagAssertionDao tagAssertionDao) {
 		this.tagAssertionDao = tagAssertionDao;
 	}
 
+	/**
+	 * Returns a DAO object for tag property operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public TagPropertyDao getTagPropertyDao() {
 		return tagPropertyDao;
 	}
 
+	/**
+	 * Sets the DAO object for tag property operations.
+	 * 
+	 * @param tagPropertyDao an instantiated DAO object
+	 */
 	public void setTagPropertyDao(TagPropertyDao tagPropertyDao) {
 		this.tagPropertyDao = tagPropertyDao;
 	}
@@ -115,67 +142,147 @@ public class GnizrDao implements Serializable{
 		// no code;
 	}
 
+	/**
+	 * Returns a DAO object for bookmark operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public BookmarkDao getBookmarkDao() {
 		return bookmarkDao;
 	}
 
+	/**
+	 * Sets the DAO object for bookmark operations.
+	 * 
+	 * @param bookmarkDao an instantiated DAO object
+	 */
 	public void setBookmarkDao(BookmarkDao bookmarkDao) {
 		this.bookmarkDao = bookmarkDao;
 	}
 
+	/**
+	 * Returns a DAO object for link operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public LinkDao getLinkDao() {
 		return linkDao;
 	}
 
+	/**
+	 * Sets the DAO object for link operations.
+	 * 
+	 * @param linkDao an instantiated DAO object
+	 */
 	public void setLinkDao(LinkDao linkDao) {
 		this.linkDao = linkDao;
 	}
 
+	/**
+	 * Returns a DAO object for tag operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public TagDao getTagDao() {
 		return tagDao;
 	}
 
+	/**
+	 * Sets the DAO object for tag operations.
+	 * 
+	 * @param tagDao an instantiated DAO object
+	 */
 	public void setTagDao(TagDao tagDao) {
 		this.tagDao = tagDao;
 	}
 
+	/**
+	 * Returns a DAO object for user operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public UserDao getUserDao() {
 		return userDao;
 	}
 
+	/**
+	 * Sets the DAO object for user operations.
+	 * 
+	 * @param userDao an instantiated DAO object
+	 */
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
+	/**
+	 * Returns a DAO object for feed subscription operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public FeedSubscriptionDao getFeedSubscriptionDao() {
 		return feedSubscriptionDao;
 	}
 
+	/**
+	 * Sets the DAO object for feed subscription operations.
+	 * 
+	 * @param feedSubscriptionDao an instantiated DAO object
+	 */
 	public void setFeedSubscriptionDao(FeedSubscriptionDao feedSubscriptionDao) {
 		this.feedSubscriptionDao = feedSubscriptionDao;
 	}
 	
+	/**
+	 * Returns a DAO object for for-user operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public ForUserDao getForUserDao ()
 	{
 		return forUserDao;
 	}
 	
+	/**
+	 * Sets the DAO object for for-user operations.
+	 * 
+	 * @param forUserDao an instantiated DAO object
+	 */
 	public void setForUserDao (ForUserDao forUserDao) {
 		this.forUserDao = forUserDao;
 	}
 
+	/**
+	 * Returns a DAO object for folder operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public FolderDao getFolderDao() {
 		return folderDao;
 	}
 
+	/**
+	 * Sets the DAO object for folder operations.
+	 * 
+	 * @param folderDao an instantiated DAO object
+	 */
 	public void setFolderDao(FolderDao folderDao) {
 		this.folderDao = folderDao;
 	}
 
+	/**
+	 * Returns a DAO object for geometry marker operations. 
+	 * 
+	 * @return <code>null</code> if the associated DAO is undefined. 
+	 */
 	public GeometryMarkerDao getGeometryMarkerDao() {
 		return geometryMarkerDao;
 	}
 
+	/**
+	 * Sets the DAO object for geometry marker operations.
+	 * 
+	 * @param geometryMarkerDao an instantiated DAO object
+	 */
 	public void setGeometryMarkerDao(GeometryMarkerDao geometryMarkerDao) {
 		this.geometryMarkerDao = geometryMarkerDao;
 	}
