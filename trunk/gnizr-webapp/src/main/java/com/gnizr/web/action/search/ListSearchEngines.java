bc57bd0f8029f17c0fd79b96e32b1605c1129e4a
harry.chen@gmail.com <harry.chen@gmail.com@df594a80-b839-0410-a48c-17488be76373>
2008-02-06 21:24:29 +0000

    merged OpenSearchDirectory configuration into gnizr-config.xml

--- trunk/gnizr-webapp/src/main/java/com/gnizr/web/action/search/ListSearchEngines.java
+++ trunk/gnizr-webapp/src/main/java/com/gnizr/web/action/search/ListSearchEngines.java
@@ -8,8 +8,9 @@ import org.apache.log4j.Logger;
 import com.gnizr.core.search.OpenSearchDirectory;
 import com.gnizr.core.search.OpenSearchService;
 import com.gnizr.web.action.AbstractLoggedInUserAction;
+import com.gnizr.web.util.GnizrConfiguration;
 
-public class ListSearchEngines extends AbstractLoggedInUserAction{
+public class ListSearchEngines extends AbstractLoggedInUserAction {
 
 	/**
 	 * 
@@ -17,12 +18,12 @@ public class ListSearchEngines extends AbstractLoggedInUserAction{
 	private static final long serialVersionUID = -7674261654917938966L;
 	private Logger logger = Logger.getLogger(ListSearchEngines.class);
 
-	private OpenSearchDirectory openSearchDirectory;
-	
+	private static OpenSearchDirectory openSearchDirectory;
+
 	private List<OpenSearchService> services;
 
 	private String q;
-	
+
 	public String getQ() {
 		return q;
 	}
@@ -36,35 +37,57 @@ public class ListSearchEngines extends AbstractLoggedInUserAction{
 	}
 
 	public void setOpenSearchDirectory(OpenSearchDirectory openSearchDirectory) {
-		this.openSearchDirectory = openSearchDirectory;
+		ListSearchEngines.openSearchDirectory = openSearchDirectory;
 	}
 
 	@Override
-	protected boolean isStrictLoggedInUserMode() {		
+	protected boolean isStrictLoggedInUserMode() {
 		return false;
 	}
 
-
 	@Override
 	protected String go() throws Exception {
-		logger.debug("ListSearchEngine.go(): loggedInUser=" + getLoggedInUser());
-		if(openSearchDirectory.getWebApplicationUrl() == null){
-			String prefixUrl = getGnizrConfiguration().getWebApplicationUrl();
-			openSearchDirectory.setWebApplicationUrl(prefixUrl);
-		}
-		openSearchDirectory.init();
+		logger.debug("ListSearchEngine.go(): loggedInUser="
+						+ getLoggedInUser());
+
+		openSearchDirectory = getOpenSearchDirectory();
+
 		List<OpenSearchService> allServices = openSearchDirectory.getServices();
 		services = new ArrayList<OpenSearchService>();
-		if(getLoggedInUser() == null && allServices.isEmpty() == false){
-			for(OpenSearchService srv : allServices){
-				if(srv.isLoginRequired() == false){
+		if (getLoggedInUser() == null && allServices.isEmpty() == false) {
+			for (OpenSearchService srv : allServices) {
+				if (srv.isLoginRequired() == false) {
 					services.add(srv);
 				}
-			}			
-		}else if (allServices.isEmpty() == false){
+			}
+		} else if (allServices.isEmpty() == false) {
 			services.addAll(allServices);
 		}
 		return SUCCESS;
 	}
 
+	private synchronized OpenSearchDirectory getOpenSearchDirectory() {
+		if (openSearchDirectory == null) {			
+			GnizrConfiguration c = getGnizrConfiguration();
+			if (c == null) {
+				logger.error("Missing GnizrConfiguration");
+				throw new RuntimeException("Missing GnizrConfiguration");
+			}
+			if (c.getOpenSearchServices() == null
+					|| !(c.getOpenSearchServices() instanceof List)) {
+				final String m = "OpenSearchServices variable is undefined in the GnizrConfiguraiton";
+				logger.error(m);
+				throw new RuntimeException(m);
+			}
+			openSearchDirectory = new OpenSearchDirectory(c
+					.getOpenSearchServices());
+			if (openSearchDirectory.getWebApplicationUrl() == null) {
+				String prefixUrl = getGnizrConfiguration()
+						.getWebApplicationUrl();
+				openSearchDirectory.setWebApplicationUrl(prefixUrl);
+			}
+			openSearchDirectory.init();
+		}
+		return openSearchDirectory;
+	}
 }
