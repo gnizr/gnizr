bcec5b4e53b1da384bff597ba700203d8d5835ce
harry.chen@gmail.com <harry.chen@gmail.com@df594a80-b839-0410-a48c-17488be76373>
2008-03-05 19:23:57 +0000

    init impl of IndexBookmark action.

---
+++ trunk/gnizr-webapp/src/test/java/com/gnizr/web/action/search/TestIndexBookmark.java
@@ -0,0 +1,27 @@
+package com.gnizr.web.action.search;
+
+import org.dbunit.dataset.IDataSet;
+
+import com.gnizr.core.web.junit.GnizrWebappTestBase;
+
+import junit.framework.TestCase;
+
+public class TestIndexBookmark extends GnizrWebappTestBase {
+
+  
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
