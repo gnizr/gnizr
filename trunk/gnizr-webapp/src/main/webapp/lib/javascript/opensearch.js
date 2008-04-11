32e9c37e99826e7e0351efcbc3afb7ec8272978b
harry.chen@gmail.com <harry.chen@gmail.com@df594a80-b839-0410-a48c-17488be76373>
2008-04-11 21:20:49 +0000

    css change. avoid displaying bookmark description in full HTML.

--- trunk/gnizr-webapp/src/main/webapp/lib/javascript/opensearch.js
+++ trunk/gnizr-webapp/src/main/webapp/lib/javascript/opensearch.js
@@ -202,7 +202,7 @@ SearchExecutor.prototype.fetchMoreData = function(){
          var summaryElm = MochiKit.DOM.P({'class':'entryDescription'});
          summaryElm.innerHTML = summary;
          var entryElm = MochiKit.DOM.LI(null,
-            MochiKit.DOM.A({'class':'entryTitle','href':link,'target':'_blank'},title),
+            MochiKit.DOM.A({'class':'entryTitle link-title','href':link,'target':'_blank'},title),
             editLinkElm,
             ' ',
             viewDetailElm,
