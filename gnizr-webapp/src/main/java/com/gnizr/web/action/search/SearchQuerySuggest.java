1c1c75eed171e8f4b81df603441d659b6b94c93e
harry.chen <harry.chen@gmail.com>
2008-04-11 19:13:31 +0000

    added query suggest logic to check whether or not a suggestion matches any doc currently in the search bookmark index.

--- gnizr-webapp/src/main/java/com/gnizr/web/action/search/SearchQuerySuggest.java
+++ gnizr-webapp/src/main/java/com/gnizr/web/action/search/SearchQuerySuggest.java
@@ -1,10 +1,16 @@
 package com.gnizr.web.action.search;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
 
+import com.gnizr.core.search.DocumentCreator;
+import com.gnizr.core.search.SearchIndexManager;
 import com.gnizr.core.search.SearchTermSuggestion;
 import com.gnizr.web.action.AbstractAction;
 
@@ -19,18 +25,24 @@ public class SearchQuerySuggest extends AbstractAction {
 
 	private String q;
 	private Map<String, String> keywords = new HashMap<String, String>();
-	
-	/*private String[] defaultsList = { "web", "web blog", "web program",
-			"webber", "class:facility", "class:facility_north" };*/
 
 	private SearchTermSuggestion searchTermSuggestion;
-	
-	
+	private SearchIndexManager searchIndexManager;
+
+	public SearchIndexManager getSearchIndexManager() {
+		return searchIndexManager;
+	}
+
+	public void setSearchIndexManager(SearchIndexManager searchIndexManager) {
+		this.searchIndexManager = searchIndexManager;
+	}
+
 	public SearchTermSuggestion getSearchTermSuggestion() {
 		return searchTermSuggestion;
 	}
 
-	public void setSearchTermSuggestion(SearchTermSuggestion searchTermSuggestion) {
+	public void setSearchTermSuggestion(
+			SearchTermSuggestion searchTermSuggestion) {
 		this.searchTermSuggestion = searchTermSuggestion;
 	}
 
@@ -46,14 +58,37 @@ public class SearchQuerySuggest extends AbstractAction {
 		this.q = q;
 	}
 
+	private IndexReader getIndexReader() throws CorruptIndexException, IOException {
+		File indexDir = searchIndexManager.getIndexDirectory();
+		if(indexDir != null){
+			return IndexReader.open(indexDir);
+		}
+		return null;
+	}
+
 	@Override
 	protected String go() throws Exception {
 		logger.debug("Start SearchTermSuggestion. q = " + getQ()
 				+ " keywords size: " + keywords.size());
-		if (q != null && searchTermSuggestion != null){
-			String[] results = searchTermSuggestion.suggest(q);
-			for(String s : results){
-				keywords.put(s,s);
+		if (q != null && searchTermSuggestion != null) {
+			String[] results = new String[0];
+			IndexReader idxReader = null; 
+			try{
+				idxReader = getIndexReader();
+			}catch(Exception e){
+				logger.error(e);
+			}
+			if(idxReader != null){
+				try{
+					results = searchTermSuggestion.suggest(q,idxReader,DocumentCreator.FIELD_TEXT);
+				}finally{
+					idxReader.close();
+				}
+			}else{
+				results = searchTermSuggestion.suggest(q);
+			}
+			for (String s : results) {
+				keywords.put(s, s);
 			}
 		}
 		return SUCCESS;
