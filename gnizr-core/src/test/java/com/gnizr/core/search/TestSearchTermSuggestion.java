d9c0c6aed20ad1e4ee1123d381a25a9cf61aa011
harry.chen <harry.chen@gmail.com>
2008-04-11 19:13:43 +0000

    added query suggest logic to check whether or not a suggestion matches any doc currently in the search bookmark index.

--- gnizr-core/src/test/java/com/gnizr/core/search/TestSearchTermSuggestion.java
+++ gnizr-core/src/test/java/com/gnizr/core/search/TestSearchTermSuggestion.java
@@ -1,18 +1,39 @@
 package com.gnizr.core.search;
 
-import junit.framework.TestCase;
+import java.io.File;
+import java.util.List;
 
-public class TestSearchTermSuggestion extends TestCase {
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+
+import com.gnizr.core.GnizrCoreTestBase;
+import com.gnizr.core.bookmark.BookmarkPager;
+import com.gnizr.db.dao.Bookmark;
+import com.gnizr.db.dao.DaoResult;
+import com.gnizr.db.dao.User;
+
+public class TestSearchTermSuggestion extends GnizrCoreTestBase {
 
 	private SearchIndexProfile profile;
 	private SearchSuggestIndexer indexer;
 	private SearchTermSuggestion suggest;
+	
+	private SearchIndexManager manager;
+	
+	private BookmarkPager bookmarkPager;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
 		profile = new SearchIndexProfile();
 		profile.setSearchIndexDirectory("target/testSearchTermSuggestion");
 		profile.setSearchSuggestDataFile("src/test/resources/dictionary/default.txt");
+		
+		manager = new SearchIndexManager(true);
+		manager.setProfile(profile);
+		manager.init();
+		
 		indexer = new SearchSuggestIndexer();
 		indexer.setSearchIndexProfile(profile);
 		indexer.init();
@@ -22,6 +43,7 @@ public class TestSearchTermSuggestion extends TestCase {
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		manager.destroy();
 	}
 	
 	public void testSuggest() throws Exception{
@@ -29,7 +51,42 @@ public class TestSearchTermSuggestion extends TestCase {
 		assertNotNull(results);
 		assertEquals(4,results.length);
 		results = suggest.suggest("class:");
-		assertEquals(4,results.length);
+		assertEquals(4,results.length);		
+	}
+	
+	public void testSuggestWithFreqFilter() throws Exception{
+		bookmarkPager = new BookmarkPager(getGnizrDao());
+		
+		DaoResult<Bookmark> result = bookmarkPager.pageAllBookmark(new User(2));
+		List<Bookmark> bmarks = result.getResult();
+		for(Bookmark bm : bmarks){
+			Document doc = DocumentCreator.createDocument(bm);			
+			manager.addIndex(doc);
+		}
+		
+		result = bookmarkPager.pageAllBookmark(new User(3));
+		bmarks = result.getResult();
+		
+		for(Bookmark bm : bmarks){
+			Document doc = DocumentCreator.createDocument(bm);
+			manager.addIndex(doc);
+		}
+		while(manager.isIndexProcessActive() == true){
+			Thread.sleep(5000);
+		}
+		
+		File indexDir = manager.getIndexDirectory();
+		IndexReader indexReader = IndexReader.open(indexDir);
+		String[] words = suggest.suggest("test", indexReader, DocumentCreator.FIELD_TEXT);
+		assertEquals(0,words.length);
+	
+		words = suggest.suggest("full",indexReader,DocumentCreator.FIELD_TEXT);
+		assertEquals(1,words.length);
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSet(TestSearchTermSuggestion.class.getResourceAsStream("/TestSearchTermSuggestion-input.xml"));
 	}
 
 }
