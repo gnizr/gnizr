13eaadd422ca2cb7ac04b7d776067f4c91dd624d
harry.chen <harry.chen@gmail.com>
2008-04-28 19:35:46 +0000

    added support for a new configuration option: "approval"

--- gnizr-web/src/main/java/com/gnizr/web/interceptor/RegistrationPolicyInterceptor.java
+++ gnizr-web/src/main/java/com/gnizr/web/interceptor/RegistrationPolicyInterceptor.java
@@ -4,6 +4,19 @@ import com.gnizr.web.util.GnizrConfiguration;
 import com.opensymphony.xwork.ActionInvocation;
 import com.opensymphony.xwork.interceptor.Interceptor;
 
+/**
+ * An {@link com.opensymphony.xwork.interceptor.Interceptor} implementation for
+ * guarding the user registration functions. When this <code>Interceptor</code>
+ * is used on a WebWork Action, it will permit an incoming request to pass
+ * if and only if the <code>registrationPolicy</code> of 
+ * <code>GnizrConfiguration</code> is either <code>open</code> or <code>approval</code>.
+ * If <code>registrationPolicy</code> is <code>close</code>, then this interceptor
+ * immediately returns <code>close</code> and skips the invocation of the action.
+ * 
+ * @author Harry Chen
+ * @since 2.4
+ *
+ */
 public class RegistrationPolicyInterceptor implements Interceptor{
 
 /**
@@ -32,8 +45,10 @@ public class RegistrationPolicyInterceptor implements Interceptor{
 	public String intercept(ActionInvocation actionInvocation) throws Exception {
 		if(gnizrConfiguration != null){
 			String policy = gnizrConfiguration.getRegistrationPolicy();
-			if(policy != null && policy.equalsIgnoreCase("open")){
-				return actionInvocation.invoke();
+			if(policy != null){
+				if(policy.equalsIgnoreCase("open") || policy.equalsIgnoreCase("approval")){
+					return actionInvocation.invoke();
+				}
 			}
 			return "close";
 		}
